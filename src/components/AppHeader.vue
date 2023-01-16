3f925443f58ae3ce361c46f4417aac2ff50c3056
Nathan Richard <nathanrichard@bluewin.ch>
2023-01-16 20:07:28 +0100

    Deleted the WElcoming Page and added my Footer, which is finished, as well my Navbar, which is under construction

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="sticky-top">
+    <nav class="navbar navbar-expand-md border-b navback">
+      <RouterLink to="/"><img src="@/assets/logo.svg" alt="NR Logo. It consists of the two letters N and R written together in purple." height="40"></RouterLink>
+      <button class="navbar-toggler button-animation" type="button" data-toggle="collapse" data-target="#navbarSupportedContent23" aria-controls="navbarSupportedContent23" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="animated-icon"><span></span><span></span><span></span><span></span></div>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent23">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.html">Home</a>
+          </li>
+          <span class="spacer"></span>
+          <li class="nav-item">
+            <a class="nav-link" href="aboutme.html">About Me</a>
+          </li>
+          <span class="spacer"></span>
+          <li class="nav-item">
+            <a class="nav-link" href="projects/projects.html">Projects</a>
+          </li>
+          <span class="spacer"></span>
+          <li class="nav-item">
+            <a class="nav-link" href="private/private.html">Private</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader"
+}
+</script>
+
+<style scoped>
+.border-b {
+  border-bottom: 3px solid var(--color-border)
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -12,3 +14,5 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.min.js"
